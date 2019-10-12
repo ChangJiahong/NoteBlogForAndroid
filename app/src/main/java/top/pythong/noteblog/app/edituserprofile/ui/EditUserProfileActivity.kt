d8d9e8f704fa0ea4deeddbeea30cfd2ba727713f
ChangJiahong <2327085154@qq.com>
2019-10-12 20:01:21 +0800

    :sparkles: 添加新特性；完善用户信息编辑页面

--- app/src/main/java/top/pythong/noteblog/app/edituserprofile/ui/EditUserProfileActivity.kt
+++ app/src/main/java/top/pythong/noteblog/app/edituserprofile/ui/EditUserProfileActivity.kt
@@ -1,9 +1,13 @@
 package top.pythong.noteblog.app.edituserprofile.ui
 
 import android.os.Bundle
+import android.view.View
+import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_edit_user_profile.*
+import org.jetbrains.anko.toast
 import top.pythong.noteblog.R
+import top.pythong.noteblog.app.aboutMe.ui.AboutMeFragment
 import top.pythong.noteblog.app.login.model.LoggedInUser
 import top.pythong.noteblog.base.activity.BaseActivity
 import top.pythong.noteblog.data.constant.Constant
@@ -11,10 +15,7 @@ import top.pythong.noteblog.utils.getStringFromSharedPreferences
 
 class EditUserProfileActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_user_profile)
-    }
+    private var modifyMark = false
 
     override fun getContentView(): Int {
         return R.layout.activity_edit_user_profile
@@ -28,10 +29,33 @@ class EditUserProfileActivity : BaseActivity() {
         val userJson = getStringFromSharedPreferences(Constant.LOGGED_IN_USER)
         val user = Gson().fromJson<LoggedInUser>(userJson, LoggedInUser::class.java)
 
+        Glide.with(this).load(user.imgUrl).into(icon)
+        username.text = user.username
+        email.text = user.email
+        info.text = ""
+        age.text = user.age.toString()
 
     }
 
     override fun initData() {
+        if (modifyMark) {
+            setResult(AboutMeFragment.NEED_TO_REFRESH)
+        }
+    }
 
+    fun modifyAvatar(v: View) {
+        toast("修改头像")
+    }
+
+    fun modifyUsername(v: View) {
+        toast("修改用户名")
+    }
+
+    fun modifyInfo(v: View) {
+        toast("修改签名")
+    }
+
+    fun modifyAge(v: View) {
+        toast("修改年龄")
     }
 }
